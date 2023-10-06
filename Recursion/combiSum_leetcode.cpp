def79f88dad00b1cea7962f5bc69a4ade2b622f0
PriyanshuG712 <priyanshug712@gmail.com>
2023-10-06 19:45:47 +0530

    06-10-2023

--- Recursion/combiSum_leetcode.cpp
+++ Recursion/combiSum_leetcode.cpp
@@ -1,31 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-
     void f(int ind, vector<int> &ds, int target, vector<int> &arr, vector<vector<int>> &ans)
     {
-        if(ind==arr.size())
+        if (ind == arr.size())
         {
-            if(target==0)
+            if (target == 0)
             {
                 ans.push_back(ds);
             }
             return;
         }
-        if(arr[ind]<=target)
+        if (arr[ind] <= target)
         {
             ds.push_back(arr[ind]);
-            f(ind,ds,target-arr[ind],arr,ans);
+            f(ind, ds, target - arr[ind], arr, ans);
             ds.pop_back();
         }
-        f(ind+1,ds,target,arr,ans);
+        f(ind + 1, ds, target, arr, ans);
     }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+    {
         vector<vector<int>> ans;
         vector<int> ds;
-        f(0,ds,target,candidates,ans);
+        f(0, ds, target, candidates, ans);
         return ans;
     }
 };
@@ -33,16 +34,17 @@ public:
 int main()
 {
     Solution s1;
-    int target=7;
-    vector<int> p {2,3,6,7};
-    vector<vector<int>> ans=s1.combinationSum(p,target);
-    cout<<"the Combinations are:"<<endl;
-    for (int i = 0; i < ans.size(); i++) 
+    int target = 7;
+    vector<int> p{2, 3, 6, 7};
+    vector<vector<int>> ans = s1.combinationSum(p, target);
+    cout << "the Combinations are:" << endl;
+    for (int i = 0; i < ans.size(); i++)
     {
-    for (int j = 0; j < ans[i].size(); j++){
-      cout << ans[i][j] << " ";
-    }
-    cout << endl;
+        for (int j = 0; j < ans[i].size(); j++)
+        {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
     }
     return 0;
-    }
+}
